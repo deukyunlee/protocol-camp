bc3ebf9207fea6d8fdcdc0c9adfa55ed884be44a
deukyunlee <dukedev1004@gmail.com>
2022-06-23 15:41:05 +0900

    test

--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"html"
-	"log"
-	"net/http"
+	"github.com/deukyunlee/protocol-camp/user"
+	"github.com/gofiber/fiber/v2"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":80", nil))
-
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Welcome to Project!!")
 }
 
-//docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
+func Routers(app *fiber.App) {
+	app.Get("/users", user.GetUsers)
+	app.Get("/user/:id", user.GetUser)
+	// app.Get("/usertest/:id", user.GetUser2)
+	app.Post("/user", user.SaveUser)
+	app.Delete("/user/:id", user.DeleteUser)
+	app.Put("/user/:id", user.UpdateUser)
+}
+
+func main() {
+	// result := test.Test_plus(3,5)
+	// fmt.Println(result)
+	user.InitialMigration()
+	app := fiber.New()
+	app.Get("/", hello)
+	Routers(app)
+	app.Listen(":3000")
+}
